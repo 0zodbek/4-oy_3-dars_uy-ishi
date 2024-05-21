efc63736c1a986e9ade48096b69d82a7badbbb5c
0zodbek <ozpdbekov@gmail.com>
2024-05-22 01:17:15 +0500

    16 issues

---
+++ app.js
@@ -0,0 +1,249 @@
+// 1-masala
+
+// alert("a sonni kiriting va u sonni musbat ekanligini tekshiring;");
+
+// let a = +prompt('a sonni kiiriting;');
+
+// if ( a > 0 ) {
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 2-masala
+
+// alert(" a sonni kiritng va uni toq ekanligini tekshiring;");
+
+// let a = +prompt('a sonni kiriting;');
+
+// if ( a % 2 == 1 ){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 3-masala
+
+// alert("a sonni kiriting va uni juft ekanligini tekshiring;");
+
+// if ( a % 2 == 0 ) {
+
+//  console.log(true);   
+// }else{
+// console.log(false);
+// };
+
+
+// 4-masala
+
+// alert("ikkita son kiriting va jumlani rost ekanligini tekshiring a > 2  ,    b <= 3 ");
+
+// let a = +prompt("a ni kiriting");
+
+// let b = +prompt("b ni kiriting");
+
+// if (a > 2 && b <= 3) {
+
+//   console.log(true);
+
+// } else {
+
+//   console.log(false);
+
+// }
+
+// 5-masala
+
+// alert("ikkita son kiriting va jumlani rost ekanligini tekshiring a >=0  ,   b < -2  ;");
+
+// let a = +prompt("a ni kiriting");
+
+// let b = +prompt("b ni kiriting");
+
+// if (a >= 0 && b < -2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 6-masala
+
+// alert(" uchta son kiriting va jumlani rost ekanligini tekshiring a <= b <= c ");
+
+// let a = +prompt(" a ni kiriting; ");
+
+// let b = +prompt(" b ni kiriting; ");
+
+// let c = +prompt(" c ni kiriting; ");
+
+// if (a <= b && b <= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// };
+
+// 7-masala
+
+// alert("uchta son kiriting va rostligini tekshiring ' a < b ; b < c '  ");
+
+// let a = +prompt(" a ni kiriting;");
+
+// let b = +prompt(" b ni kiriting;");
+
+// let c = +prompt(" c ni kiriting;");
+
+// if (a < b && b < c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 8-masala
+
+// alert("ikkita son kiriting va har ikkisi toq ekanligini tekshiring;");
+
+// let a = +prompt("a ni kiriting;");
+
+// let b = +prompt("b ni kiriting;");
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// } ;
+
+
+// 9-masala
+
+// alert("ikkita son kiiting hech bolmaganda bitasi toqligini tekshiing;");
+
+// let a = +prompt("a sonini kiriting;");
+
+// let b = +prompt("b sonini kiriting;");
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// } ;
+
+
+// 10-masala
+
+// alert(" ikkita son kiiting bu sonlarning faqat bittasi toqligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// if ( (a % 2 == 1 && b % 2 == 0)||(a % 2 == 0 && b % 2 == 1) ){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+// 11-masala
+
+// alert("ikkita son kiriting ular har ikkisi yoki toq yoki juft ekanligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// if ((a % 2 == 1  &&  b % 2 == 1) || (a % 2 == 0 && b % 2 == 0)){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 12-masala
+
+// alert("3 ta son kiriting ularning hammasi musbat ekanligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// let c = +prompt('c sonini kiriting;');
+
+// if (a < 0 && b < 0 && c < 0 ){
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 13-masala
+
+// alert("uchta son kiriting hech bolmaganda bittasi musbat ekanligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// let c = +prompt('c sonini kiriting;');
+
+// if ( a > 0 || b > 0 || c > 0 ){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 14-masala
+
+// alert("uchta son kiriting ulardan faqat bittasi musbat ekanligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// let c = +prompt('c sonini kiriting;');
+
+// if ( (a > 0 && b < 0 && C < 0) ||  (a < 0 && b > 0 && c < 0)||   (a < 0 && b < 0 && c > 0) ){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 15-masala
+
+// alert("uchta son kiriting ulardan faqat ikkitasi musbat ekanligini tekshiring;");
+
+// let a = +prompt('a sonini kiriting;');
+
+// let b = +prompt('b sonini kiriting;');
+
+// let c = +prompt('c sonini kiriting;');
+
+// if ( (a > 0 && b > 0 && C < 0) ||  (a > 0 && b < 0 && c > 0)||   (a < 0 && b > 0 && c > 0) ){
+
+// console.log(true);
+// }else{
+// console.log(false);
+// };
+
+
+// 16-masala
+
+// alert("son kiiting bu son ikki xonali juft ekanligini tekshiring;");
+
+// let x = +prompt('x sonni kiriting;');
+
+// if ( x >= 10 && x < 99 && x % 2 == 0 ){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
